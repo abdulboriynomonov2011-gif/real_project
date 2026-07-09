d4f0b499cb97ee84c4421b25ef7d74859e1c5064
abdulboriynomonov2011-gif <abdulboriynomonov2011@gmail.com>
2026-07-09 14:57:24 +0500

    t

--- loyiha/src/App.jsx
+++ loyiha/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
+import Card from './Card/Card.jsx'
+
 
 const App = () => {
   return (
     <div className='ota_div' >
       
 <Navbar/>
+
+<Card/>
+
 
     </div>
   )
---
+++ loyiha/src/Card/Card.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import "./Card.css"
+
+const Card = () => {
+  return (
+    <div className='ota_card' >
+      <div className="card_menu">
+<h1>Popular Dishes</h1>
+
+
+<div className="tugmalar">
+ 
+ <button> --| </button>
+<button> |-- </button>
+
+</div>
+
+      </div>
+
+<div className="card">
+
+<div className="box">
+
+<img src="" alt="" />
+
+<p>Barbecue Shish kebab Shashlik Skewer</p>
+
+
+
+</div>
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Card
